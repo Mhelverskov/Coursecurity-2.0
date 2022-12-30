6187ba12b4a96974e508c264c32693f938aa1c94
Magnus <Helverskov96@gmail.com>
2022-12-30 09:45:24 +0100

    Commit all

---
+++ frontend/src/Components/InformationSection.tsx
@@ -0,0 +1,57 @@
+import { useContext } from "react";
+
+import Button from "react-bootstrap/Button"
+
+import { AppContext } from "../Interfaces";
+
+import "./css/InformationSection.css";
+
+/**
+ * InformationSection Component
+ * Rendering individual pages as part of an Intro or Outro
+ * @returns Single page/slide of Intro Outro
+ */
+function InformationSection(props: any) {
+  const appContext = useContext(AppContext);
+  function DataSection(props: any) {
+    return (
+      <>
+        <div className="infoSectionTop">
+          <h3>{props.title}</h3>
+        </div>
+        <div
+          className="infoSection"
+          dangerouslySetInnerHTML={{ __html: props.paragraph }}
+        />
+      </>
+    );
+  }
+  if (props.splash) {
+    return (
+      <div>
+        <Button variant="secondary" className="switch" onClick={props.close}>
+          Return to current challenge
+        </Button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        {(appContext.data.currentChallenge! < appContext.data.challenges!.length - 1) && (
+        <Button onClick={appContext.data.nextChallenge} className="switch">
+          {appContext.data.currentChallenge === (appContext.data.challenges!.length -2)
+            ? "Start next challenge!"
+            : "Start next challenge!"}
+        </Button>)
+        }</div>
+    );
+  } else {
+    return (
+      <div className="informationContainer">
+        <DataSection
+          title={props.data[props.page - 1].title}
+          paragraph={props.data[props.page - 1].paragraph}
+          img={props.data[props.page - 1].img}
+        />
+      </div>
+    );
+  }
+}
+
+export default InformationSection;
---
+++ pia-webmail/webmail/src/features/overview/WriteEMailDialog.tsx
@@ -0,0 +1,96 @@
+import { Alert, Button, Collapse, Dialog, DialogActions, DialogContent, DialogTitle, Divider, Stack, TextField } from "@mui/material";
+import { useEffect } from "react";
+import { useState, useTransition } from "react";
+import { useAppSelector } from "../../app/hooks";
+import { Mail } from "../appstate/appSlice";
+
+
+type WriteEMailDialogProps = {
+    open?: boolean
+    onClose: () => void
+    onSend: (mail: Mail) => void
+}
+
+const WriteEMailDialog = ({open = false, onClose, onSend}: WriteEMailDialogProps) : JSX.Element => {
+    
+    
+    const [mail, setMail] = useState({ subject: "", recipient: "", content: "" });
+    
+    const [showAlert, setShowAlert] = useState(false)
+    
+    useEffect(() => {
+        if (showAlert) {
+            const timeId = setTimeout(() => {
+                onSend(mail)
+                setShowAlert(false)
+            }, 5000)
+            
+            return () => {
+                clearTimeout(timeId)
+            }
+        } 
+    }, [showAlert,mail,onSend])
+    
+
+    const setSubject = (value: string) => setMail({ ...mail, subject: value });
+    const setRecipient = (value: string) => setMail({ ...mail, recipient: value });
+    const setContent = (value: string) => setMail({ ...mail, content: value });
+
+    const handleSend = () => setShowAlert(true)
+
+    return (
+        <Dialog maxWidth="sm" fullWidth open={open} onClose={onClose}>
+            <DialogTitle>
+                New E-Mail
+            </DialogTitle>
+
+            <DialogContent>
+            <Collapse in={showAlert}>
+                <Alert severity="success"  sx={{ mb: 2 }}>
+                    Email sendt
+                </Alert>
+            </Collapse>
+                <Stack spacing={2} direction="column">
+                    <TextField
+                        id="recipient"
+                        variant="standard"
+                        required
+                        label="To"
+                        placeholder="Enter the email of the recipient"
+                        value={mail.recipient}
+                        onChange={e => setRecipient(e.target.value)}
+                    />
+
+                    <TextField
+                        id="subject"
+                        variant="standard"
+                        required
+                        label="Subject"
+                        name="subject"
+                        placeholder="Subject"
+                        value={mail.subject}
+                        onChange={e => setSubject(e.target.value)}
+                    />
+                    <Divider/>
+                    <TextField
+                        id="content"
+                        variant="standard"
+                        multiline
+                        minRows={10}
+                        label="Content"
+                        name="content"
+                        value={mail.content}
+                        onChange={e => setContent(e.target.value)}
+                    />
+                </Stack>
+            </DialogContent>
+            <DialogActions>
+                <Button variant="contained" onClick={handleSend}>
+                    Send
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default WriteEMailDialog
